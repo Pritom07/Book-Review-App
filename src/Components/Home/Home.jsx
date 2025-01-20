d8298d7c856c7e3ea862eff46861c928af42d340
Pritom07 <pritommohajon4545@gmail.com>
2025-01-21 00:34:45 +0600

    top banner complite

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,29 @@
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+  const navigate = useNavigate();
+  const gotoList = () => {
+    navigate("/listedbooks");
+  };
   return (
-    <div className="max-w-7xl mx-auto">
-      <h1>Home page</h1>
+    <div className="max-w-7xl mx-auto bg-slate-100 py-7 pl-5 rounded-3xl mt-2">
+      <section className="flex justify-between items-center">
+        <div>
+          <h1 className="text-6xl font-semibold">
+            Books to Freshen up <br /> your bookshelf
+          </h1>
+          <button
+            type="button"
+            className="bg-[#23BE0A] text-white mt-6 px-4 py-3 rounded-lg text-lg font-extrabold hover:scale-95"
+            onClick={gotoList}
+          >
+            View The List
+          </button>
+        </div>
+        <div>
+          <img src="images/topbanner.png" className="w-96" />
+        </div>
+      </section>
     </div>
   );
 };
